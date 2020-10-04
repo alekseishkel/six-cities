305d0dd5192fdaac343bbd04181973196aa090fb
alex <lexashkel@gmail.com>
2020-10-04 17:04:39 +0300

    main-page.e2e.test.js fixed

--- src/components/main-page/main-page.e2e.test.js
+++ src/components/main-page/main-page.e2e.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import MainPage from './main-page.jsx';
 
 Enzyme.configure({adapter: new Adapter()});
 
-// разобраться с тестом
-
 it(`MainPage correctly renders after relaunch`, () => {
   const clickHandler = jest.fn();
-  const app = shallow(
+  const app = mount(
       <MainPage
         func={clickHandler}
       />
@@ -18,8 +16,8 @@ it(`MainPage correctly renders after relaunch`, () => {
 
   const headers = app.find(`.place-card__info`);
   headers.forEach((header) => {
-    header.simulate(`click`);
+    header.simulate(`click`, {preventDefault() {}});
   });
 
-  expect(clickHandler).toHaveBeenCalledTimes(0);
+  expect(clickHandler).toHaveBeenCalledTimes(4);
 });
