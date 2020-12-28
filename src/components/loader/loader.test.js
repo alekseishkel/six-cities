f6cfa2e2f9591fb9a82f1a630c2d5a9f4c4c8ada
Alex <lexashkel@gmail.com>
2020-12-28 16:26:16 +0300

    test for loader

---
+++ src/components/loader/loader.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Loader from './loader.jsx';
+
+it(`Loader correctly renders after relaunch`, () => {
+  const tree = renderer.create(
+      <Loader />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
